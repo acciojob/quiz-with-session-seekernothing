0980fb8e4b18956d6e1107d7e3a16cb3a12955e1
ABHISHEK BIRADAR <162307955+seekernothing@users.noreply.github.com>
2025-03-07 22:07:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here.
-
 // Do not change code below this line
 // This code will just display the questions to the screen
+let questioncontainer = document.getElementById("questions")
 const questions = [
   {
     question: "What is the capital of France?",
@@ -29,28 +29,56 @@ const questions = [
     answer: "Ottawa",
   },
 ];
-
 // Display the quiz questions and choices
 function renderQuestions() {
-  for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    const questionElement = document.createElement("div");
-    const questionText = document.createTextNode(question.question);
-    questionElement.appendChild(questionText);
-    for (let j = 0; j < question.choices.length; j++) {
-      const choice = question.choices[j];
-      const choiceElement = document.createElement("input");
-      choiceElement.setAttribute("type", "radio");
-      choiceElement.setAttribute("name", `question-${i}`);
-      choiceElement.setAttribute("value", choice);
-      if (userAnswers[i] === choice) {
-        choiceElement.setAttribute("checked", true);
-      }
-      const choiceText = document.createTextNode(choice);
-      questionElement.appendChild(choiceElement);
-      questionElement.appendChild(choiceText);
-    }
-    questionsElement.appendChild(questionElement);
-  }
+	questions.forEach((q,index)=>{
+		let div = document.createElement("div")
+		div.innerHTML = `<p> ${q.question}  </p>`
+		q.choices.forEach((option)=>{
+			div.innerHTML+= `<input type="radio" name="q${index}" value="${option}"> ${option}`
+		})
+		questioncontainer.appendChild(div)
+	})
 }
+  
 renderQuestions();
+// code for storing the answer in session storage 
+document.querySelectorAll("input[type='radio']").forEach((radio)=>{
+	radio.addEventListener("change",()=>{
+		let progress={}
+			
+		document.querySelectorAll("input[type='radio']:checked").forEach((radio)=>{
+			progress[radio.name]= radio.value;
+		})
+		
+		sessionStorage.setItem("progress",JSON.stringify(progress))
+	})
+})
+// code for loading saved answers
+function reload(){
+	let savedAnswers = JSON.parse(sessionStorage.getItem("progress"));
+	if(savedAnswers){
+		document.querySelectorAll("input[type='radio']").forEach(radio => {
+            if (savedAnswers[radio.name] === radio.value) {
+                radio.checked = true;
+            }
+	})
+}
+}
+reload()
+
+// code to calculate score & storeing it in localstorage
+
+document.getElementById("submit").addEventListener("click",()=>{
+	let score = 0;
+	let savedAnswers = JSON.parse(sessionStorage.getItem("progress")||{});
+	questions.forEach((q,index)=>{
+		if (savedAnswers[`q${index}`] === q.answer) {
+			score++
+		}
+	})
+	document.getElementById("score").innerText= `Your score is ${score} out of 5`
+	localStorage.setItem("score",score)
+})
+
+
